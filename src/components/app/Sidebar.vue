6467e006d54ea7b058c7b407e413c130b22acda7
Ноженькин Василий Васильевич <nozhenkin.vasiliy@gmail.com>
2020-12-22 12:32:09 +0500

    Added date filter and dropdown plagin init

--- src/components/app/Sidebar.vue
+++ src/components/app/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul class="sidenav app-sidenav" :class="{open: value}">
+  <ul class="sidenav app-sidenav" :class="{ open: value }">
     <router-link
       v-for="link in links"
       :key="link.url"
@@ -8,21 +8,21 @@
       :to="link.url"
       :exact="link.exact"
     >
-      <a href="#" class="waves-effect waves-orange pointer">{{ link.title }}}</a>
+      <a href="#" class="waves-effect waves-orange pointer">{{ link.title }}</a>
     </router-link>
   </ul>
 </template>
 <script>
 export default {
-    props: ['value'],
-    data: () => ({
-        links: [
-            {'title': 'Счет', 'url': '/', 'exact': true},
-            {'title': 'История', 'url': '/history'},
-            {'title': 'Планирование', 'url': '/planning'},
-            {'title': 'Новая запись', 'url': '/record'},
-            {'title': 'Категории', 'url': '/categories'}
-        ]
-    })
-}
+  props: ["value"],
+  data: () => ({
+    links: [
+      { title: "Счет", url: "/", exact: true },
+      { title: "История", url: "/history" },
+      { title: "Планирование", url: "/planning" },
+      { title: "Новая запись", url: "/record" },
+      { title: "Категории", url: "/categories" }
+    ]
+  })
+};
 </script>
