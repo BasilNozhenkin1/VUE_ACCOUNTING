db10ee360d17969975894710ced14ceb01222740
Vasiliy Nozhenkin <nozhenkin.vasiliy@mail.ru>
2021-01-15 17:01:07 +0500

    implemented main page, currency filter added

--- src/layouts/EmptyLayout.vue
+++ src/layouts/EmptyLayout.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="grey darken-1 empty-layout">
-    <router-view/>
-  </div>
+	<div class="grey darken-1 empty-layout">
+		<router-view />
+	</div>
 </template>
 <script>
 import messages from "@/utils/mesages";
@@ -13,9 +13,8 @@ export default {
 	},
 	watch: {
 		error(fbError) {
-			console.log(fbError);
-			this.$error(messages[fbError.code] || 'Произошла ошибка');
+			this.$error(messages[fbError.code] || "Произошла ошибка");
 		}
 	}
-};	
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ import router from "./router";
 import store from "./store";
 
 import dateFilter from "@/filters/date.filter";
+import currencyFilter from "@/filters/currency.filter";
 import messagePlugin from "@/utils/message.plugin";
+import Loader from "@/components/Loader";
 
 import firebase from "firebase";
 import "firebase/auth";
@@ -19,30 +21,30 @@ Vue.config.productionTip = false;
 
 Vue.use(Vuelidate);
 Vue.use(messagePlugin);
-Vue.filter('date', dateFilter);
+Vue.filter("date", dateFilter);
+Vue.filter("currency", currencyFilter);
+Vue.component("Loader", Loader);
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBQlr-LaOpd6s1t_PwGKb1K3v-NK5dCG4Y",
-  authDomain: "tidal-mote-255505.firebaseapp.com",
-  databaseURL: "https://tidal-mote-255505-default-rtdb.firebaseio.com",
-  projectId: "tidal-mote-255505",
-  storageBucket: "tidal-mote-255505.appspot.com",
-  messagingSenderId: "1061735442479",
-  appId: "1:1061735442479:web:8ecd6c3f9944e17e913eda",
-  measurementId: "G-JCT2YPLBBR"
-
+	apiKey: process.env.VUE_APP_FIREBASE,
+	authDomain: "tidal-mote-255505.firebaseapp.com",
+	databaseURL: "https://tidal-mote-255505-default-rtdb.firebaseio.com",
+	projectId: "tidal-mote-255505",
+	storageBucket: "tidal-mote-255505.appspot.com",
+	messagingSenderId: "1061735442479",
+	appId: "1:1061735442479:web:8ecd6c3f9944e17e913eda",
+	measurementId: "G-JCT2YPLBBR"
 });
 
 let app;
 
 firebase.auth().onAuthStateChanged(() => {
-  if (!app) {
-    app = new Vue({
-      router,
-      store,
-      Vuelidate,
-      render: h => h(App)
-    }).$mount("#app");
-  }
+	if (!app) {
+		app = new Vue({
+			router,
+			store,
+			Vuelidate,
+			render: h => h(App)
+		}).$mount("#app");
+	}
 });
-
